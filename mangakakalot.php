51440aa8f3db696a979e0124acd07e7c699d5b9f
mangaowlio <136756665+mangaowlio@users.noreply.github.com>
2023-06-19 12:18:03 -0700

    Add files via upload

---
+++ mangakakalot.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Manga</title>
+</head>
+<style>
+    body
+    {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        width: 100%;
+        height: 100vh;
+    }
+    h1
+    {
+        font-size: 60px;
+    }
+    a
+    {
+        /* margin-top: 20px; */
+        text-decoration: none;
+        cursor: pointer;
+        font-size: 25px;
+    }
+</style>
+<body>
+    <?php echo "<h1>Welcome To Mangakakalot</h1>"; ?>
+    <?php echo '<a href="https://mangakakalot.app/">Mangakakalot</a>'; ?>
+</body>
+</html>
